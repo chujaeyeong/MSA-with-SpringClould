01778f8c8ded549ef3698a43bd56967fdf79b6a3
jae yeong <chujy1224@gmail.com>
2025-05-08 01:27:21 +0900

    3. API Gateway Service - Spring Clould Gateway 를 활용하여 프로젝트별 라우팅 설정, 필터 설정 (application.yml 파일 내에서 / config 클래스 안에서)

---
+++ apigateway-service/src/main/java/com/example/apigateway_service/config/FilterConfig.java
@@ -0,0 +1,26 @@
+package com.example.apigateway_service.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// application.yml 에서 라우팅 필터값을 설정하지 않고 Config 클래스를 만들어서 설정하는 방법
+//@Configuration
+public class FilterConfig {
+
+//    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/first-service/**")
+                        .filters(f -> f.addRequestHeader("first-request", "first-request-header")
+                                        .addResponseHeader("first-response", "first-response-header"))
+                        .uri("http://localhost:7081"))
+                .route(r -> r.path("/second-service/**")
+                        .filters(f -> f.addRequestHeader("second-request", "second-request-header")
+                                .addResponseHeader("second-response", "second-response-header"))
+                        .uri("http://localhost:7082"))
+                .build();
+    }
+
+}
