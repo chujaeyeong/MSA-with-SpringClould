51b5688c693db9cd447cbc891bbc038d37e8749a
jae yeong <chujy1224@gmail.com>
2025-05-10 00:57:54 +0900

    3. API Gateway Service - Spring Clould Gateway 필터 클래스를 만들어서 커스텀 필터를 적용

---
+++ apigateway-service/src/main/java/com/example/apigateway_service/filter/CustomFilter.java
@@ -0,0 +1,40 @@
+package com.example.apigateway_service.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+
+    public CustomFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom Pre Filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest(); // reactive 포함된 걸로 import
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Custom PRE filter: request id -> {}", request.getId());
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Custom POST filter: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+    public static class Config {
+        // Put the configration properties
+
+    }
+
+}
